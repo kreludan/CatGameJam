a78e0d195888741952aa9ae7b6e4aa4d606d114a
Nate Fuller <ohhnate@gmail.com>
2023-11-13 20:14:20 -0500

    added bullet collision

--- scripts/core/weaponry/BaseBullet.cs
+++ scripts/core/weaponry/BaseBullet.cs
@@ -7,17 +7,10 @@ public partial class BaseBullet : Area2D
     [Export]
     protected float Speed = 300;
     protected Vector2 Direction = Vector2.Zero;
-    
-    public override void _Ready()
-    {
-        // Connect the body_entered signal to the OnBulletAreaEntered method
-        //Connect("body_entered", this, nameof(OnBulletAreaEntered));
-    }
 
     public override void _Process(double delta)
     {
         Translate(Direction * Speed * (float)delta);
-        // Check for collisions or other logic here
     }
 
     public virtual void SetDirection(Vector2 direction)
@@ -25,15 +18,10 @@ public partial class BaseBullet : Area2D
         Direction = direction.Normalized();
     }
 
-    // Handle any collision logic here
-    private void OnBulletAreaEntered(object area)
-    {
-        // You can customize the collision logic in the derived classes
-        QueueFree();
-    }
-
+    //collision
     public void _on_area_entered(Area2D area)
     {
         GD.Print("Collide");
+        QueueFree();
     }
 }
